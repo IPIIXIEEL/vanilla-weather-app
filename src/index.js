e39fff4c40a17844479bb7f01acc8b494f66e522
Pru <87931775+IPIIXIEEL@users.noreply.github.com>
2021-11-14 16:01:57 +1100

    Added  search functionality

--- src/index.js
+++ src/index.js
@@ -31,19 +31,37 @@ function showWeather(response) {
     let windElement = document.querySelector("#wind");
     let humiElement = document.querySelector("#humi");
     let dtElement = document.querySelector("#dt");
+    let iconImage = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
     humiElement.innerHTML = `${response.data.main.humidity}%`;
     dtElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconImage.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconImage.setAttribute("alt", `${response.data.weather[0].description}`);
+    //console.log(response.data.weather[0].icon);
 }
 
-let apiKey = "3332c3356f67e41032d8159fde794731";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Mudgee&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(showWeather);
+function search(city) {
+    let apiKey = "3332c3356f67e41032d8159fde794731";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 //let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 //let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 //axios.get(url).then(showWeather);
+//console.log(cityInputElement.value);
